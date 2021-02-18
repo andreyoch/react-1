a5985fdfd8de28175f4676f72d650bf8f82f4e25
Andrzej Ciszewski <at100991@student.sgh.waw.pl>
2021-02-18 11:27:32 +0100

    before functionall component

--- src/components/UsersPage/User/User.jsx
+++ src/components/UsersPage/User/User.jsx
@@ -1,10 +1,12 @@
 import s from "./User.module.css";
+import userAvatar from "../../../assets/images/userAvatar.png";
+
 const User = (props) => {
   return (
     <div className={s.userBody}>
       <div className={s.userLeftBlock}>
         <img
-          src="https://img.icons8.com/plasticine/2x/user-male-circle.png"
+          src={props.userAvatar ? props.userAvatar : userAvatar}
           alt="userImage"
         />
         <button onClick={props.followUnfollow} className={s.followBtn}>
@@ -14,11 +16,6 @@ const User = (props) => {
       <div className={s.userRightBlock}>
         <div className={s.nameDescBlock}>
           <div className={s.userName}>{props.name}</div>
-          <div className={s.userDesc}>{props.desc}</div>
-        </div>
-        <div className={s.userLocation}>
-          <div className={s.country}>{props.country},</div>
-          <div className={s.city}>{props.city}</div>
         </div>
       </div>
     </div>
--- src/components/UsersPage/UsersPage.jsx
+++ src/components/UsersPage/UsersPage.jsx
@@ -1,60 +1,33 @@
 import s from "./UsersPage.module.css";
 import User from "./User/User";
+import axios from "axios";
 
 const UsersPage = (props) => {
-  if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        name: "Dmitry K",
-        country: "Belarus",
-        city: "Minsk",
-        desc: "I am looking for job now",
-        followed: true,
-      },
-      {
-        id: 2,
-        name: "Svetlana D",
-        country: "Belarus",
-        city: "Minsk",
-        desc: "I am so pretty",
-        followed: true,
-      },
-      {
-        id: 3,
-        name: "Sergei S",
-        country: "Ukraine",
-        city: "Kiev",
-        desc: "I like football!",
-        followed: false,
-      },
-      {
-        id: 4,
-        name: "Andrew T",
-        country: "USA",
-        city: "Philadelphia",
-        desc: "I am free to help you to create good video production",
-        followed: false,
-      },
-    ]);
-  }
+  const getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          props.setUsers(response.data.items);
+        });
+    }
+  };
   const userElements = props.users.map((user) => (
     <User
       key={user.id}
       name={user.name}
-      desc={user.desc}
-      country={user.country}
-      city={user.city}
       followed={user.followed ? "Unfollow" : "Follow"}
       followUnfollow={
         user.followed
           ? () => props.unfollow(user.id)
           : () => props.follow(user.id)
       }
+      userAvatar={user.photos.small}
     />
   ));
   return (
     <div className={s.usersPage}>
+      <button onClick={getUsers}>Get users</button>
       <header className={s.header}>Users</header>
       {userElements}
     </div>
